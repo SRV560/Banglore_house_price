a7edf4ce7198cfe09438dd1760d38758ded2755a
Sourav Sekhar Pradhan <89440394+Sourav-Sekhar-560@users.noreply.github.com>
2022-01-13 15:50:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 import json
 
-app = Flask(__name__,template_folder='/home/ubuntu')
+app = Flask(__name__)
 
 app.config.update(
     dict(SECRET_KEY="powerful secretkey", WTF_CSRF_SECRET_KEY="a csrf secret key")
